4028d69e4a805cc5d2efd52527f62bd4b51de84e
Michael Herold <michael.j.herold@gmail.com>
2015-11-15 16:45:53 -0600

    Add support for stateless inclusions

--- lib/module_builder/builder.rb
+++ lib/module_builder/builder.rb
@@ -13,6 +13,32 @@ module ModuleBuilder
     # @param [Module] base the module to use as a base on which to build.
     def initialize(base: Module.new)
       @module = base
+
+      add_inclusions
+    end
+
+    # Lists the modules to be included into the built module.
+    #
+    # @note This can be overridden in a subclass to automatically
+    #   include modules in the built module.
+    #
+    # @return [Array<Module>] the modules to be included into the built
+    #   module.
+    def inclusions
+      []
+    end
+
+    private
+
+    # Includes the modules listed by the builder's {#inclusions} in the
+    # built module.
+    #
+    # @note This method is called by the constructor and can be
+    #   overridden.
+    #
+    # @return [void]
+    def add_inclusions
+      inclusions.each { |inclusion| @module.__send__(:include, inclusion) }
     end
   end
 end
--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -5,4 +5,26 @@ RSpec.describe ModuleBuilder::Builder do
     builder = ModuleBuilder::Builder.new
     expect(builder.module).to be_a_kind_of Module
   end
+
+  it "includes all modules listed for inclusion in the built module" do
+    Quack = Module.new do
+      def quack
+        "quack"
+      end
+    end
+
+    including_builder = Class.new(ModuleBuilder::Builder) do
+      def inclusions
+        [Quack]
+      end
+    end
+
+    quacking_class = Class.new do
+      include including_builder.new.module
+    end
+
+    quacker = quacking_class.new
+    expect(quacker).to respond_to(:quack)
+    expect(quacker.quack).to eq("quack")
+  end
 end
